028bf411fecd951fa0d80e8b2a34223bdb7449c0
nirbhayfr <nirbhaychauhan0306@gmail.com>
2025-10-04 10:31:57 +0530

    initial commit

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,10 @@
+async function Page() {
+     return (
+          <div>
+               <h1 className="head-text mb-10">Communities</h1>
+               <p className="text-light-3">No Communities yet</p>
+          </div>
+     );
+}
+
+export default Page;
---
+++ components/shared/Bottombar.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { sidebarLinks } from "@/constants";
+import { useAuth } from "@clerk/nextjs";
+
+function Bottombar() {
+     const { userId } = useAuth();
+     const pathname = usePathname();
+
+     return (
+          <section className="bottombar">
+               <div className="bottombar_container">
+                    {sidebarLinks.map((link) => {
+                         const isActive =
+                              (pathname.includes(link.route) &&
+                                   link.route.length > 1) ||
+                              pathname === link.route;
+
+                         if (link.route === "/profile")
+                              link.route = `${link.route}/${userId}`;
+
+                         return (
+                              <Link
+                                   href={link.route}
+                                   key={link.label}
+                                   className={`bottombar_link ${
+                                        isActive && "bg-primary-500"
+                                   }`}
+                              >
+                                   <Image
+                                        src={link.imgURL}
+                                        alt={link.label}
+                                        width={24}
+                                        height={24}
+                                   />
+                                   <p className="text-light-1 text-subtle-medium max-sm:hidden">
+                                        {link.label.split(/\s+/)[0]}
+                                   </p>
+                              </Link>
+                         );
+                    })}
+               </div>
+          </section>
+     );
+}
+
+export default Bottombar;
